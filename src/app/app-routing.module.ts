1f9eb19785e628878ef794c2b3d8d1a66a378aa1
rowanabisaiutp <a.rowan.ojeda@utponiente.edu.mx>
2023-10-06 17:30:01 -0600

    cambios rowan

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { InicioComponent } from './inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/inicio', pathMatch:'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'inicio', component: InicioComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
