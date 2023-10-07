6c59005edb5f9f665f5d0c7a3c8a2183246ca03e
zeruasutp <a.irvin.suarez@utponiente.edu.mx>
2023-10-07 12:21:20 -0500

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { Inicio2Component } from './inicio/inicio2.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/inicio', pathMatch:'full'},
   {path: 'home', component: HomeComponent},
   {path: 'inicio', component: InicioComponent}, 
+  {path: 'inicio2', component: Inicio2Component}, 
   {path: 'nosotros', component: NosotrosComponent}
 ];
 
